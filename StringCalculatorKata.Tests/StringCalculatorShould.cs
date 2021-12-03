89b1ade56526623e2b22e1efead2c31e2b88a8f2
Raúl Gómez <rgomez@voxelgroup.net>
2021-12-03 10:38:43 +0100

    Initial commit

---
+++ StringCalculatorKata.Tests/StringCalculatorShould.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace StringCalculatorKata.Tests
+{
+    public class StringCalculatorShould
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
