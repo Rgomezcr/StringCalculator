677439b03b4f48206fc63f172d53e365a05314e7
Raúl Gómez <rgomez@voxelgroup.net>
2021-12-03 16:40:21 +0100

    refactor: generalize solution

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -9,16 +9,12 @@ namespace StringCalculatorKata
             if (number.Length == 1)
                 return Int32.Parse(number);
 
-            if (number == "0,1")
-                return 1;
-            if (number == "0,2")
-                return 2;
-            if (number == "0,3")
-                return 3;
-            if (number == "0,4")
-                return 4;
-            if (number == "0,5")
-                return 5;
+            if (number.Length == 3)
+            {
+                return Int32.Parse(number.Substring(2, 1));
+            }
+
+            
             return 0;
         }
     }
